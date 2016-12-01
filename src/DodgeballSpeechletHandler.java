925a5edaf6fad2426cee367000f3b6e3a5b1e71a
William Fischer <Bfish@Williams-MBP.users.condointernet.net>
2016-12-01 00:06:56 -0800

    Pull keys from ENV varibles to prevent them from leaking

--- src/DodgeballSpeechletHandler.java
+++ src/DodgeballSpeechletHandler.java
@@ -2,7 +2,6 @@ import java.util.Set;
 
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 import com.google.common.collect.Sets;
-import org.apache.commons.lang3.Validate;
 
 public final class DodgeballSpeechletHandler extends SpeechletRequestStreamHandler {
     private static final Set<String> supportedApplicationIds;
@@ -12,7 +11,7 @@ public final class DodgeballSpeechletHandler extends SpeechletRequestStreamHandl
          * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
          * Alexa Skill and put the relevant Application Ids in this Set.
          */
-        supportedApplicationIds = Sets.newHashSet();
+        supportedApplicationIds = Sets.newHashSet(System.getenv().get("SUPPORTED_SKILL_APP_ID"));
     }
 
     public DodgeballSpeechletHandler() {
